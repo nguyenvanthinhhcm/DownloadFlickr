bab12edd1d6e09cb78796b2baab3554eebc5aded
nguyenvanthinhhcm <41589802+nguyenvanthinhhcm@users.noreply.github.com>
2018-07-24 01:12:38 +0700

    DownloadFlickr
    DownloadFlickr

---
+++ DownloadFlickr/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace DownloadFlickr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtTag = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btSr = new System.Windows.Forms.Button();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.cbOriginal = new System.Windows.Forms.CheckBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.LInk = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Size = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.backgroundWorker3 = new System.ComponentModel.BackgroundWorker();
+            this.txtSize = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtTag
+            // 
+            this.txtTag.Location = new System.Drawing.Point(303, 7);
+            this.txtTag.Name = "txtTag";
+            this.txtTag.Size = new System.Drawing.Size(154, 20);
+            this.txtTag.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(224, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Picture Tags :";
+            // 
+            // btSr
+            // 
+            this.btSr.Location = new System.Drawing.Point(12, 5);
+            this.btSr.Name = "btSr";
+            this.btSr.Size = new System.Drawing.Size(185, 23);
+            this.btSr.TabIndex = 2;
+            this.btSr.Text = "Search";
+            this.btSr.UseVisualStyleBackColor = true;
+            this.btSr.Click += new System.EventHandler(this.btSr_Click);
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(681, 7);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(109, 20);
+            this.txtFolder.TabIndex = 3;
+            this.txtFolder.Text = "year";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(633, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Folder :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(470, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Numbers :";
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(531, 7);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(81, 20);
+            this.txtNum.TabIndex = 5;
+            this.txtNum.Text = "10";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Get";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(224, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "User :";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(303, 51);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(154, 20);
+            this.txtUser.TabIndex = 8;
+            // 
+            // cbOriginal
+            // 
+            this.cbOriginal.AutoSize = true;
+            this.cbOriginal.Location = new System.Drawing.Point(807, 10);
+            this.cbOriginal.Name = "cbOriginal";
+            this.cbOriginal.Size = new System.Drawing.Size(96, 17);
+            this.cbOriginal.TabIndex = 10;
+            this.cbOriginal.Text = "Original Format";
+            this.cbOriginal.UseVisualStyleBackColor = true;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.WorkerReportsProgress = true;
+            this.backgroundWorker2.WorkerSupportsCancellation = true;
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.LInk,
+            this.Size});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 93);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(980, 378);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // LInk
+            // 
+            this.LInk.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.LInk.HeaderText = "Link";
+            this.LInk.Name = "LInk";
+            // 
+            // Size
+            // 
+            this.Size.HeaderText = "Size";
+            this.Size.Name = "Size";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(807, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(185, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Get";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 479);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1004, 22);
+            this.statusStrip1.TabIndex = 13;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(13, 17);
+            this.toolStripStatusLabel1.Text = "0";
+            // 
+            // backgroundWorker3
+            // 
+            this.backgroundWorker3.WorkerReportsProgress = true;
+            this.backgroundWorker3.WorkerSupportsCancellation = true;
+            this.backgroundWorker3.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker3_DoWork);
+            // 
+            // txtSize
+            // 
+            this.txtSize.Location = new System.Drawing.Point(473, 51);
+            this.txtSize.Name = "txtSize";
+            this.txtSize.Size = new System.Drawing.Size(154, 20);
+            this.txtSize.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 501);
+            this.Controls.Add(this.txtSize);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cbOriginal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.btSr);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTag);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Download Flickr";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTag;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btSr;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.CheckBox cbOriginal;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LInk;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Size;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker3;
+        private System.Windows.Forms.TextBox txtSize;
+    }
+}
+
---
+++ DownloadFlickr/Form1.cs
@@ -0,0 +1,204 @@
+﻿using FlickrNet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DownloadFlickr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Flickr F = new Flickr();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string apikey = "keyapi";
+            F = new Flickr("keyapi", "keyapi");
+        }
+
+        private void btSr_Click(object sender, EventArgs e)
+        {
+            if (!backgroundWorker1.IsBusy) backgroundWorker1.RunWorkerAsync();
+        }
+        public string[] BiggestVersionUrl(Photo Photo)
+        {
+            string[] Org = new string[2];
+            string BiggestVersionUrl1 = string.Empty;
+            string Size = string.Empty;
+            var p = Photo;
+            if (!string.IsNullOrEmpty(p.OriginalUrl))
+            {
+                BiggestVersionUrl1 = p.OriginalUrl;
+                Size = "OriginalUrl";
+            }
+            else if (!string.IsNullOrEmpty(p.Large2048Url))
+            {
+                BiggestVersionUrl1 = p.Large2048Url;
+                Size = "OriginalUrl";
+            }
+            else if (!string.IsNullOrEmpty(p.Large1600Url))
+            {
+                BiggestVersionUrl1 = p.Large1600Url;
+                Size = "Large1600Url";
+            }
+            else if (!string.IsNullOrEmpty(p.LargeUrl))
+            {
+                BiggestVersionUrl1 = p.LargeUrl;
+                Size = "LargeUrl";
+            }
+            else if (!string.IsNullOrEmpty(p.MediumUrl))
+            {
+                BiggestVersionUrl1 = p.MediumUrl;
+                Size = "MediumUrl";
+            }
+            else if (!string.IsNullOrEmpty(p.SmallUrl))
+            {
+                BiggestVersionUrl1 = p.SmallUrl;
+                Size = "OriginalUrl";
+            }
+
+            Org[0] = BiggestVersionUrl1;
+            Org[1] = Size;
+            return Org;
+        }
+
+        public string user;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            user = txtUser.Text.Trim();
+         //   if (!backgroundWorker2.IsBusy) backgroundWorker2.RunWorkerAsync();
+
+            PhotoSearchOptions searchOptions = new PhotoSearchOptions();
+            searchOptions.UserId = "149565322@N04";
+            searchOptions.PerPage = 10;
+            searchOptions.Extras = PhotoSearchExtras.All;
+            searchOptions.Extras = PhotoSearchExtras.Views;
+            PhotoCollection microsoftPhotos = F.PhotosSearch(searchOptions);
+
+            List<Photo> allPhotos = new List<Photo>();
+            allPhotos.AddRange(microsoftPhotos);
+            foreach (var photo in allPhotos)
+            {
+                using (WebClient Client = new WebClient())
+                {
+
+                    // try
+                    {
+                        //    this.Invoke(new MethodInvoker(delegate
+                        //        {
+                        //   Client.DownloadFile(BiggestVersionUrl(photo), Application.StartupPath + "\\" + txtFolder.Text + "\\" + photo.Title + ".jpg");
+                        //}));
+                    }
+                    //  catch { }
+
+                }
+
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            PhotoSearchOptions searchOptions = new PhotoSearchOptions();
+            searchOptions.Username = "德丸．tokumaru";
+            searchOptions.PerPage = 10;
+            searchOptions.Extras = PhotoSearchExtras.All;
+            searchOptions.Extras = PhotoSearchExtras.Views;
+            PhotoCollection microsoftPhotos = F.PhotosSearch(searchOptions);
+
+            List<Photo> allPhotos = new List<Photo>();
+            allPhotos.AddRange(microsoftPhotos);
+            foreach (var photo in allPhotos)
+            {
+                using (WebClient Client = new WebClient())
+                {
+
+                    // try
+                    {
+                        //    this.Invoke(new MethodInvoker(delegate
+                        //        {
+                     //   Client.DownloadFile(BiggestVersionUrl(photo), Application.StartupPath + "\\" + txtFolder.Text + "\\" + photo.Title + ".jpg");
+                        //}));
+                    }
+                    //  catch { }
+
+                }
+
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // MessageBox.Show("DONE");
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            PhotoSearchOptions searchOptions = new PhotoSearchOptions();
+            searchOptions.UserId = "namgermans";
+            searchOptions.Extras = PhotoSearchExtras.All;
+            PhotoCollection photos = F.PhotosSearch(searchOptions);
+            List<Photo> allPhotos = new List<Photo>();
+            allPhotos.AddRange(photos);
+            foreach (var photo in allPhotos)
+            {
+                using (WebClient Client = new WebClient())
+                {
+
+                 //   Client.DownloadFile(BiggestVersionUrl(photo), Application.StartupPath + "\\" + txtFolder.Text + "\\" + photo.Title + ".jpg");
+
+                }
+
+            }
+
+            MessageBox.Show("DONE");
+        }
+
+        public int total = 0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            total = int.Parse(txtSize.Text);
+            backgroundWorker3.RunWorkerAsync();
+        }
+
+        private void backgroundWorker3_DoWork(object sender, DoWorkEventArgs e)
+        {
+            PhotoSearchOptions searchOptions = new PhotoSearchOptions();
+            searchOptions.Username = "ruanjin";
+            searchOptions.Extras = PhotoSearchExtras.All;
+            searchOptions.SafeSearch = SafetyLevel.None;
+            searchOptions.PerPage = 100;
+            int i = 0;
+            for (int j = 0; j < total; j++)
+            {
+                searchOptions.Page = j;
+                PhotoCollection photos = F.PhotosSearch(searchOptions);
+                List<Photo> allPhotos = new List<Photo>();
+                allPhotos.AddRange(photos);
+                foreach (var photo in allPhotos)
+                {
+
+                    string[] Type = BiggestVersionUrl(photo);
+                    this.Invoke(new MethodInvoker(delegate
+                    {
+                        dataGridView1.Rows.Add(Type[0]);
+                        dataGridView1[1, i].Value = Type[1];
+                    }));
+                    i++;
+
+                }
+                toolStripStatusLabel1.Text = i.ToString();
+            }
+            MessageBox.Show("DONE");
+        }
+    }
+}
